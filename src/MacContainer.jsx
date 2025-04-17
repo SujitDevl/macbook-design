374aded75f46c431eca8447ce538bf860b03497c
Sujit <pandasujit687@gmail.com>
2025-04-17 12:08:11 +0530

    laptop lid functionality

--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,15 +1,15 @@
 import { useGLTF } from "@react-three/drei";
 import React from "react";
-import * as THREE from "three"
+import * as THREE from "three";
 
 function MacContainer() {
   let model = useGLTF("./mac.glb");
-  let meshes = {}
-  model.scene.traverse(e => {
-    if(e instanceof THREE.Mesh || e instanceof THREE.Group){
-      meshes[e.name] = e;
-    }
-  })
+  let meshes = {};
+  model.scene.traverse((e) => {
+    meshes[e.name] = e;
+  });
+
+  meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
 
   return (
     <group position={[0, -10, 20]}>
