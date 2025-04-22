6d8799aa3c182bf471a2caa2a0f31b991cf82fe7
Sujit <pandasujit687@gmail.com>
2025-04-23 00:36:34 +0530

    design done

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import MacContainer from "./MacContainer";
 function App() {
   return (
     <div className="w-full h-screen">
-      <div className="absolute top-32 left-1/2 -translate-x-1/2">
-      
+      <div className="absolute text-white top-32 left-1/2 -translate-x-1/2 font-['Helvetica_Now_Display']">
+        <h3 className="text-7xl tracking-tighter">macbook pro.</h3>
       </div>
       <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
         <OrbitControls />
