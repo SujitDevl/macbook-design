914f2b70397efab68fafe4adc76a7757c70a0e5c
Sujit <pandasujit687@gmail.com>
2025-04-23 00:39:18 +0530

    context

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="w-full h-screen">
       <div className="absolute text-white top-32 left-1/2 -translate-x-1/2 font-['Helvetica_Now_Display']">
-        <h3 className="text-7xl tracking-tighter">macbook pro.</h3>
+        <h3 className="text-5xl tracking-tighter font-[700]">macbook pro.</h3>
       </div>
       <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
         <OrbitControls />
